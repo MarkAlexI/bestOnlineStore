55f3c7409c4aeedbdcffb5d1a0b945fb769e46ee
MarkAlexI <markovaleksandr108@gmail.com>
2023-12-07 10:36:09 +0200

    Added more field to schema of article.

--- back-end/src/models/articleSchema.js
+++ back-end/src/models/articleSchema.js
@@ -2,7 +2,9 @@ import mongoose from 'mongoose';
 
 const articleSchema = new mongoose.Schema({
   author: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-  content: { type: String, default: '' }
+  title: { type: String, default: 'Title' },
+  content: { type: String, default: 'Content.' },
+  image: { type: String, default: '' }
 }, { timestamps: true });
 
 const Article = mongoose.model('Article', articleSchema);
