be80692fb05c8efb5a4fc711c3c9fd3efab19617
MarkAlexI <markovaleksandr108@gmail.com>
2023-12-20 19:45:33 +0200

    Added test for getting review of product.

--- back-end/tests/productRouter.test.js
+++ back-end/tests/productRouter.test.js
@@ -1,6 +1,7 @@
 import chai from 'chai';
 import chaiHttp from 'chai-http';
 import app from '../src/server.js';
+import { MESSAGES } from '../src/utils/constants.js';
 
 chai.use(chaiHttp);
 const { expect } = chai;
@@ -27,6 +28,25 @@ describe('Product Routes', function() {
       .end((err, res) => {
         expect(res).to.have.status(200);
         expect(res).to.be.json;
+
+        done();
+      });
+  });
+
+  it('should get reviews of product by id', function(done) {
+    chai.request(app)
+      .get(`/api/review/product/${productId}`)
+      .end((err, res) => {
+        expect(res).to.have.status(200);
+        expect(res).to.be.json;
+        expect(res.body).to.be.a('object');
+        expect(res.body).to.have.property('message');
+        expect(res.body).to.have.property('text');
+        expect(res.body).to.have.property('payload');
+        expect(res.body.message).to.be.equal('success');
+        expect(res.body.text).to.be.equal(MESSAGES.REVIEWS_RETRIEVED_SUCCESSFULLY);
+        expect(res.body.payload).to.have.property('reviews');
+        expect(res.body.payload).to.be.a('object');
 
         done();
       });
