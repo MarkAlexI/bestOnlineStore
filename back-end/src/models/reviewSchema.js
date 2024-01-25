1e2f41ea3c429e191c6375cf7c8d7255c791fb9d
MarkAlexI <markovaleksandr108@gmail.com>
2024-01-25 22:45:29 +0200

    Added new fields to Review for improving like/dislike/comment.

--- back-end/src/models/reviewSchema.js
+++ back-end/src/models/reviewSchema.js
@@ -1,12 +1,42 @@
 import mongoose from 'mongoose';
 
+const answerSchema = new mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  text: {
+    type: String,
+    default: ''
+  }
+}, { timestamps: true });
+
 const reviewSchema = new mongoose.Schema({
-  user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-  product: { type: mongoose.Schema.Types.ObjectId, ref: 'Product', required: true },
-  rating: { type: Number, required: true },
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  product: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Product',
+    required: true
+  },
+  rating: {
+    type: Number,
+    required: true
+  },
   comment: { type: String },
-  likes: { type: Number, default: 0 },
-  dislikes: { type: Number, default: 0 }
+  answers: [answerSchema],
+  likes: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User'
+  }],
+  dislikes: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User'
+  }]
 }, { timestamps: true });
 
 const Review = mongoose.model('Review', reviewSchema);
