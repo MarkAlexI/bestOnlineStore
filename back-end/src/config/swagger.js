e89780435ed996d7bb4f0d91cdf7e5b67920d1d6
MarkAlexI <markovaleksandr108@gmail.com>
2025-01-20 03:07:20 +0200

    Add documentation for blog route.

--- back-end/src/config/swagger.js
+++ back-end/src/config/swagger.js
@@ -9,7 +9,49 @@ const options = {
     info: {
       title: 'API Documentation',
       version: '1.0.0',
-      description: 'Documentation for API',
+      description: 'Documentation for API of Lykke Store',
+    },
+    components: {
+      schemas: {
+        Article: {
+          type: 'object',
+          properties: {
+            author: {
+              type: 'string',
+              description: 'ID автора статті',
+            },
+            title: {
+              type: 'string',
+              description: 'Заголовок статті',
+            },
+            content: {
+              type: 'string',
+              description: 'Контент статті',
+            },
+            baseImage: {
+              type: 'string',
+              description: 'Основне зображення статті',
+            },
+            allImages: {
+              type: 'array',
+              items: {
+                type: 'string',
+              },
+              description: 'Інші зображення статті',
+            },
+            createdAt: {
+              type: 'string',
+              format: 'date-time',
+              description: 'Дата створення статті',
+            },
+            updatedAt: {
+              type: 'string',
+              format: 'date-time',
+              description: 'Дата останнього оновлення статті',
+            },
+          }
+        }
+      }
     },
     servers: [
       {
@@ -18,7 +60,7 @@ const options = {
       },
     ],
   },
-  apis: ['../routers/*.js'],
+  apis: ['./src/routers/*.js'],
 };
 
 const specs = swaggerJsdoc(options);
